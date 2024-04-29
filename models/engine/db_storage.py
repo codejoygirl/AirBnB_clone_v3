7e868db9e5af722459e6411eba0dbb5d72a3a18e
Dev.Joy <Joyegbu@gmail.com>
2024-04-30 00:50:40 +0100

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,27 +1,23 @@
-#!/usr/bin/python3
-"""
-Contains the class DBStorage
-"""
-
-import models
+from os import getenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from models.base_model import Base
 from models.amenity import Amenity
-from models.base_model import BaseModel, Base
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-from os import getenv
-import sqlalchemy
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
 
-classes = {"Amenity": Amenity, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
-
+classes = {
+    "Amenity": Amenity, "City": City,
+    "Place": Place, "Review": Review,
+    "State": State, "User": User
+}
 
 class DBStorage:
-    """interaacts with the MySQL database"""
+    """Interacts with the MySQL database"""
+    
     __engine = None
     __session = None
 
@@ -41,63 +37,52 @@ class DBStorage:
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """query on the current database session"""
+        """Query on the current database session"""
         new_dict = {}
-        for clss in classes:
-            if cls is None or cls is classes[clss] or cls is clss:
-                objs = self.__session.query(classes[clss]).all()
+        if cls:
+            if cls.__name__ in classes:
+                objs = self.__session.query(cls).all()
                 for obj in objs:
-                    key = obj.__class__.__name__ + '.' + obj.id
+                    key = "{}.{}".format(obj.__class__.__name__, obj.id)
                     new_dict[key] = obj
-        return (new_dict)
+        else:
+            for clss in classes.values():
+                objs = self.__session.query(clss).all()
+                for obj in objs:
+                    key = "{}.{}".format(obj.__class__.__name__, obj.id)
+                    new_dict[key] = obj
+        return new_dict
 
     def new(self, obj):
-        """add the object to the current database session"""
+        """Add the object to the current database session"""
         self.__session.add(obj)
 
     def save(self):
-        """commit all changes of the current database session"""
+        """Commit all changes of the current database session"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """delete from the current database session obj if not None"""
-        if obj is not None:
+        """Delete from the current database session obj if not None"""
+        if obj:
             self.__session.delete(obj)
 
     def reload(self):
-        """reloads data from the database"""
+        """Create all tables in the database (feature of SQLAlchemy)"""
         Base.metadata.create_all(self.__engine)
-        sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(sess_factory)
-        self.__session = Session
+        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        self.__session = scoped_session(Session)
 
     def close(self):
-        """call remove() method on the private session attribute"""
+        """Call remove() method on the private session attribute"""
         self.__session.remove()
 
     def get(self, cls, id):
-        """ return the specified object by id
-        """
-        if cls in classes.keys():
-            objs = self.__session.query(eval(cls))
-            for elem in objs:
-                if (elem.id == id):
-                    return (elem)
-        return (None)
+        """Return the specified object by id"""
+        return self.__session.query(cls).get(id)
 
     def count(self, cls=None):
-        """ return number of specified class
-        """
-        count = 0
-        if cls is None:
-            for clss in classes:
-                objs = self.__session.query(classes[clss]).all()
-                for obj in objs:
-                    count = count + 1
-            return (count)
+        """Return the number of objects in storage"""
+        if cls:
+            return self.__session.query(cls).count()
         else:
-            if cls in classes.keys():
-                objs = self.__session.query(eval(cls))
-                for obj in objs:
-                    count = count + 1
-            return (count)
+            return sum(self.__session.query(cls).count() for cls in classes.values())
